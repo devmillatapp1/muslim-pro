03097b50c750fd17945ebd2fe3d4ec49ca1f3e83
Tayeb-Ali <elteyab.hassan@gmail.com>
2023-09-06 12:34:52 +0300

    fix2 add new function

---
+++ lib/core/translations/translation.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'package:muslim/core/translations/ar.dart';
+import 'package:muslim/core/translations/en.dart';
+
+class HisnAppTranslation extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {"ar": ar, "en": en};
+}

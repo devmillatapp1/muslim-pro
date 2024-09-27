6caadcd27c82caff01918ef79c56c3586e4d53f3
Tayeb-Ali <Eltayeb.ali119@gmail.com>
2024-09-27 08:39:18 +0300

    rm ios

--- lib/core/translations/translation.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:muslim/core/translations/ar.dart';
-import 'package:muslim/core/translations/en.dart';
-
-class HisnAppTranslation extends Translations {
-  @override
-  Map<String, Map<String, String>> get keys => {"ar": ar, "en": en};
-}

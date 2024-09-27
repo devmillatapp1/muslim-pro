06e12a1721988b85b9cd3eb6b09d77b8913736cf
Tayeb-Ali <Eltayeb.ali119@gmail.com>
2024-09-27 08:42:45 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,9 @@
-import 'dart:developer';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:muslim/app/app.dart';
-import 'package:muslim/app/init_services.dart';
-
-import 'firebase_options.dart';
+import 'package:muslim/app.dart';
+import 'package:muslim/init_services.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await initServices();
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-  );
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-  final connectivityResult = await Connectivity().checkConnectivity();
 
-  if (connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi) {
-    try {
-      Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-      final fcmToken = FirebaseMessaging.instance.getToken();
-      await FirebaseMessaging.instance.setAutoInitEnabled(true);
-      await FirebaseMessaging.instance.subscribeToTopic("news");
-      log("FCMToken $fcmToken");
-    } catch (e) {
-      log(e.toString());
-    }
-  } else {
-    log("internet available");
-  }
-
-  runApp(const MyApp());
+  runApp(const App());
 }
